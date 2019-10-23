4053d74fe873e2014bcfb7d9902cf35e08394d4a
Vit_sh <vithits@yandex.ru>
2019-10-23 17:10:45 +0300

    создана структура Question

---
+++ Millionaire-GB/Millionaire-GB/Data.swift
@@ -0,0 +1,11 @@
+//
+//  Data.swift
+//  Millionaire-GB
+//
+//  Created by Vitaly_Shishlyannikov on 23.10.2019.
+//  Copyright © 2019 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ Millionaire-GB/Millionaire-GB/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Millionaire-GB
+//
+//  Created by Vitaly_Shishlyannikov on 23.10.2019.
+//  Copyright © 2019 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let answer1 : String
+    let answer2 : String
+    let answer3 : String
+    let answer4 : String
+    let correctAnswer : String
+    let helpOfFriend: String
+    let helpOfAudience: String
+}
+
