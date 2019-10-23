67f519f1326e2e18e2d6d9d3d6e5342dfea1fc42
Vit_sh <vithits@yandex.ru>
2019-10-23 15:50:21 +0300

    создан проект Millionaire-GB

---
+++ Millionaire-GB/Millionaire-GB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Millionaire-GB
+//
+//  Created by Vitaly_Shishlyannikov on 23.10.2019.
+//  Copyright © 2019 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
