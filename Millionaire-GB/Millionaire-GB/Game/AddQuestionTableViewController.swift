87551de0752582a07a339058e2949e4c10eec4aa
Vit_sh <vithits@yandex.ru>
2019-10-31 12:12:03 +0300

    добавление вопросов перенесено в builder, из него возвращется массив, кторый сохраняется в AddedQuestionsCareTaker

--- Millionaire-GB/Millionaire-GB/Game/AddQuestionTableViewController.swift
+++ Millionaire-GB/Millionaire-GB/Game/AddQuestionTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddQuestionTableViewController: UITableViewController {
+final class AddQuestionTableViewController: UITableViewController {
     
     @IBOutlet weak var questionText: UITextField!
     @IBOutlet weak var answer1Text: UITextField!
@@ -19,15 +19,17 @@ class AddQuestionTableViewController: UITableViewController {
     @IBOutlet weak var saveQuestionButton: UIBarButtonItem!
     @IBOutlet weak var addQuestionButton: UIBarButtonItem!
     
-    var addedQuestionsCaretaker = AddedQuestonsCaretaker()
+    private var addedQuestionsCaretaker = AddedQuestonsCaretaker()
     
-    var addedQuestions = [Question]() {
+    private var addedQuestions = [Question]() {
         didSet {
             addedQuestionsCaretaker.save(addedQuestions: self.addedQuestions)
         }
     }
     
-    var questionsCount = 1
+    private var questionsCount = 1
+    
+    private let addedQuestionsBuilder = AddedQuestionBuilder()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,40 +43,8 @@ class AddQuestionTableViewController: UITableViewController {
     
     @objc func saveButtonPressed (sender: UIButton) {
         
-        var questions = [Question]()
+        self.addedQuestions = addedQuestionsBuilder.build(questionsCount: questionsCount, tableView: self.tableView)
         
-        for row in 0...questionsCount {
-            
-            let indexPath = IndexPath(row: row, section: 0)
-            let cell = tableView.cellForRow(at: indexPath) as? QuestionTableViewCell
-            
-            if let questionText = cell?.questionTextfield.text, !questionText.isEmpty,
-               let answer1Text = cell?.answer1Textfield.text, !answer1Text.isEmpty,
-               let answer2Text = cell?.answer2Textfield.text, !answer2Text.isEmpty,
-               let answer3Text = cell?.answer3Textfield.text, !answer3Text.isEmpty,
-               let answer4Text = cell?.answer4Textfield.text, !answer4Text.isEmpty,
-               let correctAnswerNumber = cell?.correctAnswerNumberSwitch.selectedSegmentIndex {
-                
-                var correctAnswer: String {
-                    switch correctAnswerNumber {
-                    case 0:
-                        return answer1Text
-                    case 1:
-                        return answer2Text
-                    case 2:
-                        return answer3Text
-                    case 3:
-                        return answer4Text
-                    default:
-                        return answer1Text
-                    }
-                }
-                
-                let question = Question(question: questionText, answer1: answer1Text, answer2: answer2Text, answer3: answer3Text, answer4: answer4Text, correctAnswer: correctAnswer, helpOfFriend: "Я не знаю :(", helpOfAudience: "Все варианты по 25%")
-                addedQuestions.append(question)
-            }
-        }
-        print(questions)
         self.performSegue(withIdentifier: "MainMenuSegue", sender: self)
     }
     
